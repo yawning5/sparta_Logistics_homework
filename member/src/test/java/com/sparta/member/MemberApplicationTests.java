3a9e2e86cbd6e88740b33e5bb9483fcc02977e4f
yawning5 <yawning055@gmail.com>
2025-11-04 18:17:29 +0900

    feat: #2 Member 모듈생성
    - EurekaServerApplication
    @EnableEurekaServer 추가
    
    - ConfigApplication
    @EnableConfigServer 추가
    
    - config.application.yml
    spring.profiles.active: native 설정 추가
    
    - config-repo
    member 관련 설정파일 추가하였습니다

---
+++ member/src/test/java/com/sparta/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

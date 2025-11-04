dfac6f6768de425b0f6fdfef9eae71fce14c0791
yoo20370 <yoo20370@gmail.com>
2025-11-04 15:45:17 +0900

    init first

---
+++ hub/src/test/java/com/keepgoing/hub/HubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepgoing.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

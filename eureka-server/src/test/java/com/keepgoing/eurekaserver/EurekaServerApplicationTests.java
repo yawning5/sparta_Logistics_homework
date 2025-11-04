e28aeac3d1ef1336bef85c30a3d36a52a4df0a25
malaheaven <yunhg95@gmail.com>
2025-11-04 16:21:56 +0900

    feat: 모듈 삭제해서 다시 프로젝트 init

--- eureka-server/src/test/java/com/keepgoing/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/keepgoing/eurekaserver/EurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.keepgoing.eurekaserver;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EurekaServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+// package com.keepgoing.eurekaserver;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class EurekaServerApplicationTests {
+//
+// 	@Test
+// 	void contextLoads() {
+// 	}
+//
+// }

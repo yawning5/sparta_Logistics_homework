47349cae08b862ab8dc9e696442a8e7a5710716d
yoo20370 <yoo20370@gmail.com>
2025-11-08 03:56:43 +0900

    refactor(Order) : OrderRepositoryCustom 불필요한 코드 제거

--- order/src/main/java/com/keepgoing/order/infrastructure/OrderRepositoryCustom.java
+++ order/src/main/java/com/keepgoing/order/infrastructure/OrderRepositoryCustom.java
@@ -1,18 +1,14 @@
 package com.keepgoing.order.infrastructure;
 
-import com.keepgoing.order.domain.Order;
 import com.keepgoing.order.domain.OrderState;
 import java.util.Collection;
 
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface OrderRepositoryCustom {
     Page<UUID> findPendingIds(Collection<OrderState> states, Pageable pageable);
-
-    Optional<Order> findOrderForUpdate(UUID orderId);
 
     int claim(UUID orderId, OrderState beforeState, OrderState afterState);
 
--- order/src/main/java/com/keepgoing/order/infrastructure/OrderRepositoryCustomImpl.java
+++ order/src/main/java/com/keepgoing/order/infrastructure/OrderRepositoryCustomImpl.java
@@ -2,15 +2,12 @@ package com.keepgoing.order.infrastructure;
 
 import static com.keepgoing.order.domain.QOrder.*;
 
-import com.keepgoing.order.domain.Order;
 import com.keepgoing.order.domain.OrderState;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.LockModeType;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -40,18 +37,6 @@ public class OrderRepositoryCustomImpl implements OrderRepositoryCustom{
             .from(order)
             .where(order.orderState.in(states));
         return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
-    }
-
-    @Override
-    public Optional<Order> findOrderForUpdate(UUID orderId) {
-        Order result = queryFactory
-            .selectFrom(order)
-            .where(order.id.eq(orderId))
-            .setLockMode(LockModeType.PESSIMISTIC_WRITE)
-            .setHint("jakarta.persistence.lock.timeout", "0")
-            .fetchOne();
-
-        return Optional.ofNullable(result);
     }
 
     @Override

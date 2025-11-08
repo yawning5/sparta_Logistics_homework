1726447d884c6d85c25ad70f75e7a988c0f415d4
yoo20370 <yoo20370@gmail.com>
2025-11-08 17:04:40 +0900

    fix(Order) : CreateOrderCommand 필드 수정 (totalPrice -> price)

--- order/src/main/java/com/keepgoing/order/application/dto/CreateOrderCommand.java
+++ order/src/main/java/com/keepgoing/order/application/dto/CreateOrderCommand.java
@@ -1,6 +1,5 @@
 package com.keepgoing.order.application.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.keepgoing.order.domain.Order;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -8,25 +7,25 @@ import lombok.Builder;
 
 @Builder
 public record CreateOrderCommand (
-    @JsonProperty("supplier_id")
+
     UUID supplierId,
-    @JsonProperty("supplier_name")
+
     String supplierName,
-    @JsonProperty("receiver_id")
+
     UUID receiverId,
-    @JsonProperty("receiver_name")
+
     String receiverName,
-    @JsonProperty("product_id")
+
     UUID productId,
-    @JsonProperty("product_name")
+
     String productName,
-    @JsonProperty("quantity")
+
     Integer quantity,
-    @JsonProperty("total_price")
-    Integer totalPrice,
-    @JsonProperty("delivery_due_at")
+
+    Integer price,
+
     LocalDateTime deliveryDueAt,
-    @JsonProperty("delivery_request_note")
+
     String deliveryRequestNote
 ){
     @Override
@@ -65,8 +64,8 @@ public record CreateOrderCommand (
     }
 
     @Override
-    public Integer totalPrice() {
-        return totalPrice;
+    public Integer price() {
+        return price;
     }
 
     @Override
@@ -88,14 +87,38 @@ public record CreateOrderCommand (
         if (productId == null) throw new IllegalArgumentException("상품의 식별자는 필수값입니다.");
         if (productName == null || productName.isBlank()) throw new IllegalArgumentException("상품 이름은 필수 값입니다.");
         if (quantity == null) throw new IllegalArgumentException("상품 수량은 필수값입니다.");
-        if (totalPrice == null) throw new IllegalArgumentException("총 가격은 필수값입니다.");
+        if (price == null) throw new IllegalArgumentException("가격은 필수값입니다.");
         if (deliveryDueAt == null) throw new IllegalArgumentException("납품 기간은 필수값입니다.");
         ;
         LocalDateTime now = LocalDateTime.now();
+        int totalPrice = calculateTotalPrice();
 
         return Order.create(
             supplierId, supplierName, receiverId, receiverName, productId, productName,
             quantity, totalPrice, now, deliveryDueAt ,deliveryRequestNote
         );
+    }
+
+    public static CreateOrderCommand create(
+        UUID supplierId, String supplierName, UUID receiverId, String receiverName, UUID productId, String productName,
+        Integer quantity, Integer price, LocalDateTime deliveryDueAt, String deliveryRequestNote
+    ) {
+
+        return CreateOrderCommand.builder()
+            .supplierId(supplierId)
+            .supplierName(supplierName)
+            .receiverId(receiverId)
+            .receiverName(receiverName)
+            .productId(productId)
+            .productName(productName)
+            .quantity(quantity)
+            .price(price)
+            .deliveryDueAt(deliveryDueAt)
+            .deliveryRequestNote(deliveryRequestNote)
+            .build();
+    }
+
+    private int calculateTotalPrice() {
+        return price * quantity;
     }
 }

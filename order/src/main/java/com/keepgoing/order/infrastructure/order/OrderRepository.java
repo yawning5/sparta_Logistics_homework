d42a49e1334c09709c01dccb90d378a35760db73
yoo20370 <yoo20370@gmail.com>
2025-11-09 19:18:39 +0900

    feat(Order) : 주문 상태를 PAID로 변경하는 JPQL 쿼리 추가

--- order/src/main/java/com/keepgoing/order/infrastructure/order/OrderRepository.java
+++ order/src/main/java/com/keepgoing/order/infrastructure/order/OrderRepository.java
@@ -1,9 +1,33 @@
 package com.keepgoing.order.infrastructure.order;
 
 import com.keepgoing.order.domain.order.Order;
+import com.keepgoing.order.domain.order.OrderState;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, UUID>, OrderRepositoryCustom {
 
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(
+    """
+      UPDATE Order o 
+          SET o.orderState = :after, 
+              o.version = o.version + 1, 
+              o.updatedAt = :now 
+      WHERE o.id = :orderId 
+          AND o.orderState = :before  
+          AND o.version = :version 
+    """
+    )
+    int updateOrderStateToPaidForPayment(
+        @Param("orderId") UUID orderId,
+        @Param("before") OrderState before,
+        @Param("after") OrderState after,
+        @Param("version") Long version,
+        @Param("now")LocalDateTime now
+    );
 }

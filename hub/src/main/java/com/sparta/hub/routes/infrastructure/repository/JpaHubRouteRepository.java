4b12c6fbbb620b96fda77951ee4335e50b51193b
An-jihyun <anjh322@gmail.com>
2025-11-11 15:53:57 +0900

    feature: 허브 검색 레포지토리 확장

--- hub/src/main/java/com/sparta/hub/routes/infrastructure/repository/JpaHubRouteRepository.java
+++ hub/src/main/java/com/sparta/hub/routes/infrastructure/repository/JpaHubRouteRepository.java
@@ -1,15 +1,15 @@
 package com.sparta.hub.routes.infrastructure.repository;
 
 import com.sparta.hub.routes.domain.entity.HubRoute;
-import com.sparta.hub.routes.domain.repository.HubRouteRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface JpaHubRouteRepository extends JpaRepository<HubRoute, UUID>, HubRouteRepository {
+public interface JpaHubRouteRepository extends JpaRepository<HubRoute, UUID>, JpaSpecificationExecutor<HubRoute> {
     Optional<HubRoute> findByOriginHubIdAndDestinationHubId(UUID originHubId, UUID destinationHubId);
     Optional<HubRoute> findByIdAndDeletedAtIsNull(UUID id);
 }

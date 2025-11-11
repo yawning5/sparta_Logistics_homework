f536898833ceea8f6349cc4c191e2608d1d35488
An-jihyun <anjh322@gmail.com>
2025-11-11 15:35:46 +0900

    feature: 허브 경로 레포지토리 생성

---
+++ hub/src/main/java/com/sparta/hub/routes/domain/repository/HubRouteRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.hub.routes.domain.repository;
+
+import com.sparta.hub.routes.domain.entity.HubRoute;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface HubRouteRepository {
+    HubRoute save(HubRoute route);
+    Optional<HubRoute> findById(UUID id);
+    Optional<HubRoute> findByOriginHubIdAndDestinationHubId(UUID originHubId, UUID destinationHubId);
+}

8fc964c4c05f9ebc267dfb724693f4cebf5b677f
An-jihyun <anjh322@gmail.com>
2025-11-11 15:59:54 +0900

    chore: 허브 경로 jpa 레포에 코드 가독성 개선용 주석 추가

--- hub/src/main/java/com/sparta/hub/routes/infrastructure/repository/JpaHubRouteRepository.java
+++ hub/src/main/java/com/sparta/hub/routes/infrastructure/repository/JpaHubRouteRepository.java
@@ -10,6 +10,8 @@ import java.util.UUID;
 
 @Repository
 public interface JpaHubRouteRepository extends JpaRepository<HubRoute, UUID>, JpaSpecificationExecutor<HubRoute> {
+    // 출발 허브와 도착 허브로 경로 단건 조회 (중복체크용)
     Optional<HubRoute> findByOriginHubIdAndDestinationHubId(UUID originHubId, UUID destinationHubId);
+    //Soft Delete 되지 않은 ID 기준 단건 조회
     Optional<HubRoute> findByIdAndDeletedAtIsNull(UUID id);
 }

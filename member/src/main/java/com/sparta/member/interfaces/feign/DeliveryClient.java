df622b4de4f9e6b7202a1bd5dfba88b2640b16ef
yawning5 <yawning055@gmail.com>
2025-11-12 17:41:45 +0900

    fix: 프로젝트 전반적 기능검사(미완)
    해결된 문제
    - 허브 서비스 등록 문제
    - Jwt Claim 타입 불일치 문제
    - Member 에서 feign 클라이언트로 보낼때 jwt 를 헤더에 담고 있지 않았던 문제
    
    해결해야할 문제
    - LocalDateTime 관련 파싱문제
    - DeliveryClient 와 MemberService 간의 통신문제

--- member/src/main/java/com/sparta/member/interfaces/feign/DeliveryClient.java
+++ member/src/main/java/com/sparta/member/interfaces/feign/DeliveryClient.java
@@ -4,10 +4,12 @@ import com.sparta.member.interfaces.feign.dto.request.DeliveryManRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "delivery-service")
 public interface DeliveryClient {
 
     @PostMapping("/v1/delivery-persons")
-    void register(@RequestBody DeliveryManRequestDto deliveryManRequestDto);
+    void register(@RequestBody DeliveryManRequestDto deliveryManRequestDto,
+        @RequestHeader("Authorization") String token);
 }

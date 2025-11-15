4359d94df972c1b40db972935a3bac646456f720
yoo20370 <yoo20370@gmail.com>
2025-11-16 02:29:00 +0900

    feat  주문을 위한 findById, Save를 메서드를 추가 및 도메인 엔티티와 영속성 엔티티를 위한 Mapper 추가

--- order/src/main/java/com/keepgoing/order/application/repository/OrderRepository.java
+++ order/src/main/java/com/keepgoing/order/application/repository/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.keepgoing.order.application.repository;
 
 import com.keepgoing.order.domain.model.Order;
+import java.util.UUID;
 
 public interface OrderRepository {
+
+    Order findById(UUID orderId);
 
     Order save(Order order);
 
--- order/src/main/java/com/keepgoing/order/infrastructure/persistence/repository/order/OrderRepositoryImpl.java
+++ order/src/main/java/com/keepgoing/order/infrastructure/persistence/repository/order/OrderRepositoryImpl.java
@@ -2,17 +2,36 @@ package com.keepgoing.order.infrastructure.persistence.repository.order;
 
 import com.keepgoing.order.application.repository.OrderRepository;
 import com.keepgoing.order.domain.model.Order;
+import com.keepgoing.order.infrastructure.persistence.mapper.OrderMapper;
+import com.keepgoing.order.infrastructure.persistence.order.entity.OrderEntity;
+import com.keepgoing.order.presentation.exception.NotFoundOrderException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class OrderRepositoryImpl implements OrderRepository {
 
-    private OrderJpaRepository orderJpaRepository;
+    private final OrderJpaRepository orderJpaRepository;
+
+    @Override
+    public Order findById(UUID orderId) {
+
+        OrderEntity orderEntity = orderJpaRepository.findById(orderId).orElseThrow(
+            () -> new NotFoundOrderException("아이디에 해당하는 주문을 찾을 수 없습니다.")
+        );
+
+        return OrderMapper.toDomain(orderEntity);
+    }
 
     @Override
     public Order save(Order order) {
-        
 
-        return order;
+        OrderEntity orderEntity = OrderMapper.toEntityForCreate(order);
+
+        orderEntity = orderJpaRepository.save(orderEntity);
+
+        return OrderMapper.toDomain(orderEntity);
     }
 }

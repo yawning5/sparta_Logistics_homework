043740dbab53625a4feed0bc8f3c5a6ccee1ed36
An-jihyun <anjh322@gmail.com>
2025-11-17 14:40:54 +0900

    feature: 허브 생성 이벤트 동기 리스너 추가

---
+++ hub/src/main/java/com/sparta/hub/routes/application/event/HubEventListener.java
@@ -0,0 +1,16 @@
+package com.sparta.hub.routes.application.event;
+
+import com.sparta.hub.routes.domain.events.HubCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class HubEventListener {
+
+    @EventListener
+    public void handle(HubCreatedEvent event) {
+        log.info("허브 생성 이벤트 수신 - 허브 ID: {}", event.getHubId());
+    }
+}
